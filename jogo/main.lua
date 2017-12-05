4042237f3a2d97597bbdd1bec9ba55506a460fd7
Carlos Vinicios Martins Rocha <vinicios_martins2011@hotmail.com>
2017-12-05 20:06:41 -0300

    Panelinhas Full Fight

--- jogo/main.lua
+++ jogo/main.lua
@@ -117,7 +117,6 @@ function pular(key) --realiza a ação do pulo com keypressed
     end
   end
 end
-<<<<<<< Updated upstream
 
 function renderizarMovimento()
   love.graphics.setColor(255, 255, 255) --define a cor devido a imagem ser transparente e o fundo vai interferir na cor do personagem
@@ -137,8 +136,6 @@ function renderizarMovimento()
     love.graphics.draw( imgPulo, posX, posY, 0, -1, 1, 27, 0 )
   end
 end
-=======
->>>>>>> Stashed changes
 --fim do controle de movimento
 
 --controle de golpes
